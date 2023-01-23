fba4c669798dd934198649eecad2fcb7e69a7dd6
Maura <maurakummer@yahoo.com>
2023-01-23 11:55:41 +0100

    create pages and routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import ForgotPassword from './pages/ForgotPassword';
+import InterviewQuestions from './pages/InterviewQuestions';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 
 function App() {
-  return (
-    <>
-    <h1>Hello World!!</h1>
-    </>
-  );
+    return (
+        <>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/forgot-password" element={<ForgotPassword />} />
+                    <Route path="/interview-questions" element={<InterviewQuestions />} />
+                    <Route path="/sign-in" element={<SignIn />} />
+                    <Route path="/sign-up" element={<SignUp />} />                   
+                </Routes>
+            </Router>
+        </>
+    );
 }
 
 export default App;
